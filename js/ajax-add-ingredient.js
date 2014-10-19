66ed529945a56cf563c19c850408a75d88e011c3
david.binda <david.binovec@gmail.com>
2014-10-19 16:48:42 +0200

    Encapsulate all javascript into immedia function

--- js/ajax-add-ingredient.js
+++ js/ajax-add-ingredient.js
@@ -1,14 +1,15 @@
-jQuery(document).ready(function($) {	
-	$('.tagadd').click(function(){		
-		var data = {
-			action: 'wp_pizzeria_add_ingredient',
-			addIngredientNonce: wp_pizzeria_addIngredient.addIngredientNonce,
-			postID: wp_pizzeria_addIngredient.postID,
-			tag: $('#new-tag-wp_pizzeria_ingredient').val()
-		};
-		jQuery.post(ajaxurl, data, function(response){
-			$('#new-tag-wp_pizzeria_ingredient').val('');
+(function($) {
+	$(document).ready(function () {
+		$('.tagadd').click(function () {
+			var data = {
+				action            : 'wp_pizzeria_add_ingredient',
+				addIngredientNonce: wp_pizzeria_addIngredient.addIngredientNonce,
+				postID            : wp_pizzeria_addIngredient.postID,
+				tag               : $('#new-tag-wp_pizzeria_ingredient').val()
+			};
+			$.post(ajaxurl, data, function (response) {
+				$('#new-tag-wp_pizzeria_ingredient').val('');
+			});
 		});
-		
 	});
-});
+})(jQuery);
--- js/filter-pizzas.js
+++ js/filter-pizzas.js
@@ -1,68 +1,48 @@
-function filterPizzas( ingredience ){
-	
-	if (jQuery('ul.wp-pizzeria-ingredienceFilter input.'+ingredience).is(':checked')){
-		jQuery('table.pizzas tbody tr').not('.'+ingredience).hide();	
-	}else{
-		jQuery('table.pizzas tbody tr').not('.'+ingredience).show();
-		jQuery('ul.wp-pizzeria-ingredienceFilter input:checked').each( function(){
-			jQuery('table.pizzas tbody tr').not('.'+jQuery(this).attr('class')).hide();	
-		} )	
-	}	
-	/**
-	jQuery('table.pizzas tbody tr.'+ingredience).find('input.'+ingredience).each(function(){
-		if ( jQuery(this).attr('checked') == 'checked' ){
-				jQuery(this).removeAttr('checked');	
-			}else{
-				jQuery(this).attr('checked', 'checked');	
+(function($) {
+	var filterPizzas = function(ingredience) {
+
+		if (jQuery('ul.wp-pizzeria-ingredienceFilter input.' + ingredience).is(':checked')) {
+			jQuery('table.pizzas tbody tr').not('.' + ingredience).hide();
+		} else {
+			jQuery('table.pizzas tbody tr').not('.' + ingredience).show();
+			jQuery('ul.wp-pizzeria-ingredienceFilter input:checked').each(function () {
+				jQuery('table.pizzas tbody tr').not('.' + jQuery(this).attr('class')).hide();
+			})
+		}
+		return;
+	};
+
+	$(document).ready(function () {
+		$('ul.wp-pizzeria-ingredienceFilter li img').click(function () {
+			$(this).parents('li').find('input').each(function () {
+				if ($(this).attr('checked') == 'checked') {
+					$(this).removeAttr('checked');
+				} else {
+					$(this).attr('checked', 'checked');
+				}
+				var ingredience = $(this).siblings('input').attr('class');
+				filterPizzas(ingredience);
+			});
+		});
+		$('ul.wp-pizzeria-ingredienceFilter li input').change(function () {
+			var ingredience = $(this).attr('class');
+			filterPizzas(ingredience);
+		});
+		$('.pizzas .description').addClass('hidden');
+		$('.pizza-title').click(function (e) {
+			e.preventDefault();
+			if (!$(this).hasClass('withdesc')) {
+				$('.withdesc').removeClass('withdesc');
 			}
-	});**/	
-	return;
-}
-jQuery(document).ready(function($){	
-	$('ul.wp-pizzeria-ingredienceFilter li img').click(function(){
-		$(this).parents('li').find('input').each(function(){
-			if ( $(this).attr('checked') == 'checked' ){
-				$(this).removeAttr('checked');	
-			}else{
-				$(this).attr('checked', 'checked');	
+			$(this).toggleClass('withdesc');
+			$('.appended-description').remove();
+			if ($(this).hasClass('withdesc')) {
+				var desc = $(this).parents('tr').find('td.description div').html();
+				$(this).parents('tr').after('<tr class="appended-description" style="display: none;"><td colspan="7">' + desc + '</td>');
+				$('.appended-description').show('slow', 'swing', function () {
+					;
+				});
 			}
-			var ingredience = $(this).siblings('input').attr('class');
-			filterPizzas( ingredience );
 		});
 	});
-	$('ul.wp-pizzeria-ingredienceFilter li input').change(function(){
-		var ingredience = $(this).attr('class');
-		filterPizzas( ingredience );
-	});	
-	/*
-	$('table.pizzas tbody tr input.wp-pizzeria-ingredience').change(function(){
-		var ingredience = $(this).attr('class').replace('wp-pizzeria-ingredience ', '');
-		$('ul.wp-pizzeria-ingredienceFilter li input.'+ingredience).each(function(){
-			if ( $(this).attr('checked') == 'checked' ){
-				$(this).removeAttr('checked');	
-			}else{
-				$(this).attr('checked', 'checked');	
-			}			
-			filterPizzas( ingredience );			
-		});
-		if ( $(this).attr('checked') == 'checked' ){
-			$(this).removeAttr('checked');	
-		}else{
-			$(this).attr('checked', 'checked');	
-		}
-	});*/
-	$('.pizzas .description').addClass('hidden');
-	$('.pizza-title').click(function(e){		
-		e.preventDefault();	
-		if( !$(this).hasClass( 'withdesc' ) ){
-			$('.withdesc').removeClass('withdesc');
-		}
-		$(this).toggleClass('withdesc');
-		$('.appended-description').remove();				
-		if( $(this).hasClass( 'withdesc' ) ){
-			var desc = $(this).parents('tr').find('td.description div').html();		
-			$(this).parents('tr').after('<tr class="appended-description" style="display: none;"><td colspan="7">'+desc+'</td>');		
-			$('.appended-description').show('slow', 'swing', function(){;});	
-		}				
-	});
-});
+})(jQuery);
